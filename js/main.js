c62cd2b5634e72633312e70d2916ae12ab2c3c09
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-07-26 12:04:19 +0000

    Add js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 // getElementById(id)
 // querySelector(selector)
 // querySelectorAll(selector)
+
+// const titleElement = document.getElementById('title');
+
+// console.log(titleElement);
+
+// const infoJsTitle = document.querySelector('#info-js h2');
+
+// console.log(infoJsTitle.textContent);
+
+// let allDiv = document.querySelectorAll('div');
+
+// for (let index = 0; index < allDiv.length; index++) {
+// 	console.log(allDiv[index]);
+// }
